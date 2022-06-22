2298856d3682f1c3df2709e5dc3cce510743845d
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-06-22 12:15:12 +0200

    Feat: Exercise Solved

---
+++ CommandLineArt/script.js
@@ -0,0 +1,11 @@
+const figlet = require('figlet');
+
+
+figlet("Hi there!", function(err, data) {
+    if(err){
+        console.log("Error: ", err);
+        return;
+    }
+    console.log(data);
+})
+
